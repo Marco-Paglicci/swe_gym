13d228756835e6385a59deb26d59ace324283b7a
Riccardo Fantechi <161381784+Riccardo1451@users.noreply.github.com>
2024-12-23 00:00:24 +0100

    added employee class

---
+++ models/Employee.java
@@ -0,0 +1,31 @@
+package models;
+
+public class Employee {
+    private String ID; //TODO:magari farlo int ??
+    private String nome;
+    private String cognome;
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+}
